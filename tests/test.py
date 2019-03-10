eef8b3ca9ee68fa0a615d43dcddd428b44cc6cd0
Karik Isichei <karik.isichei@gmail.com>
2019-03-10 18:15:52 +0000

    blah

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,7 @@ class SomeTest(unittest.TestCase):
     Test packages utilities functions
     """
     print("hey-ya")
+    print("hey-you")
     def test_test(self):
         self.assertEqual(hello(), "hello world")
         self.assertEqual(hi(), "hi")
